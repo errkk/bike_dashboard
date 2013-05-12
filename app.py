56083496c0c35c5cab696f1b468c4fc6d15139ce
Eric George <eric@pokelondon.com>
2013-05-12 17:48:29 +0100

    reinstated

--- app.py
+++ app.py
@@ -7,12 +7,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-#    try:
-#        data = bikes()
-#    except Exception as e:
-#        return e
-#    return render_template('base.html', data=data)
-    return "hi"
+    try:
+        data = bikes()
+    except Exception as e:
+        return e
+    return render_template('base.html', data=data)
 
 if __name__ == "__main__":
     app.run('0.0.0.0', debug=True)
