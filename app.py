6803f791742b8e821d7e3e65e6268c685eb545db
Eric George <eric@pokelondon.com>
2013-05-12 17:45:53 +0100

    Trying something simple

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from data import bikes
+#from data import bikes
 
 
 app = Flask(__name__)
@@ -7,11 +7,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    try:
-        data = bikes()
-    except Exception as e:
-        return e
-    return render_template('base.html', data=data)
+#    try:
+#        data = bikes()
+#    except Exception as e:
+#        return e
+#    return render_template('base.html', data=data)
+    return "hi"
 
 if __name__ == "__main__":
     app.run('0.0.0.0', debug=True)
